fc65acd5ef19483f76bec55aa5cd925695ac5e8f
saiakhil <saiakhil90@gmail.com>
2021-12-02 10:23:53 -0500

    Extend ThreeWayRadixQuicksort from SortAPI

--- src/main/java/edu/neu/coe/info6205/sort/ThreeWayRadixQuicksort.java
+++ src/main/java/edu/neu/coe/info6205/sort/ThreeWayRadixQuicksort.java
@@ -1,12 +1,36 @@
 package edu.neu.coe.info6205.sort;
 
 import edu.neu.coe.info6205.util.SortUtils;
+import org.jetbrains.annotations.NotNull;
 
-public class ThreeWayRadixQuicksort {
+public class ThreeWayRadixQuicksort extends SortAPI<String>{
 
+    @Override
+    public void sort(@NotNull String[] arr, int from, int to) {
+        this.sort(arr, from, to,0);
+    }
 
-    public static void sort(String[] a) {
-        sort(a, 0, a.length - 1, 0);
+    @Override
+    protected void sort(@NotNull String[] arr, int from, int to, int d) {
+        if (to <= from) return;
+
+        int lt = from;
+
+        int gt = to;
+        int i = from;
+        int v = charAt(arr[from], d);
+
+        while (i <= gt) {
+            int t = charAt(arr[i], d);
+
+            if (t == v) i++;
+            else if (t < v) this.swap(arr, i++, lt++);
+            else this.swap(arr, i, gt--);
+        }
+
+        this.sort(arr, from, lt - 1, d);
+        if (v >= 0) this.sort(arr, lt, gt, d + 1); // look here how u skipped dth character wow!!!!
+        this.sort(arr, gt + 1, to, d);
     }
 
     private static int charAt(String s, int d) {
@@ -19,34 +43,34 @@ public class ThreeWayRadixQuicksort {
         } else return -1;*/
     }
 
-    private static void swap(String[] a, int i, int j) {
-        String temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-    }
+//    private static void swap(String[] a, int i, int j) {
+//        String temp = a[i];
+//        a[i] = a[j];
+//        a[j] = temp;
+//    }
 
-    private static void sort(String[] a, int low, int high, int d) {
-
-        // 3 way partitioning (using dth character)
-        if (high <= low) return;
-        int lt = low;
-        int gt = high;
-        int i = low;
-        int v = charAt(a[low], d);
-
-        while (i <= gt) {
-            int t = charAt(a[i], d);
-
-            if (t == v) i++;
-            else if (t < v) swap(a, i++, lt++);
-            else if (t > v) swap(a, i, gt--);
-        }
-
-        sort(a, low, lt - 1, d);
-        if (v >= 0) sort(a, lt, gt, d + 1); // look here how u skipped dth character wow!!!!
-        sort(a, gt + 1, high, d);
-
-    }
+//    private static void sort1(String[] arr, int from, int to, int d) {
+//
+//        // 3 way partitioning (using dth character)
+//        if (to <= from) return;
+//        int lt = from;
+//        int gt = to;
+//        int i = from;
+//        int v = charAt(arr[from], d);
+//
+//        while (i <= gt) {
+//            int t = charAt(arr[i], d);
+//
+//            if (t == v) i++;
+//            else if (t < v) swap(arr, i++, lt++);
+//            else if (t > v) swap(arr, i, gt--);
+//        }
+//
+//        sort(arr, from, lt - 1, d);
+//        if (v >= 0) sort(arr, lt, gt, d + 1); // look here how u skipped dth character wow!!!!
+//        sort(arr, gt + 1, to, d);
+//
+//    }
 
 
 }
