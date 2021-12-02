4a5f9b2c92a5e4a0ece8f28ee9104eac8c808831
saiakhil <saiakhil90@gmail.com>
2021-12-02 14:52:10 -0500

    Add InsertionSortMSDTest

--- src/test/java/edu/neu/coe/info6205/sort/InsertionSortMSDTest.java
+++ src/test/java/edu/neu/coe/info6205/sort/InsertionSortMSDTest.java
@@ -1,6 +1,7 @@
 package edu.neu.coe.info6205.sort;
 
 import edu.neu.coe.info6205.util.FileReader;
+import edu.neu.coe.info6205.util.SortUtils;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -13,7 +14,7 @@ import static org.junit.Assert.assertArrayEquals;
 /**
  * @author Sai Akhil
  */
-public class DualPivotQuickSortTest {
+public class InsertionSortMSDTest {
 
     @Test
     public void sortInteger0(){
@@ -21,7 +22,7 @@ public class DualPivotQuickSortTest {
         Integer[] input = new Integer[]{4,3,5,6,1};
         Integer[] expected = Arrays.copyOf(input,input.length);
         Arrays.sort(expected);
-        new DualPivotQuicksort<Integer>().sort(input);
+        new InsertionSortMSD<Integer>().sort(input);
         assertArrayEquals(expected,input);
     }
 
@@ -29,31 +30,31 @@ public class DualPivotQuickSortTest {
     public void sortEnglish0(){
         String[] input = new String[]{"c","b","a","f","d","e"};
         String[] expected = new String[]{"a","b","c","d","e","f"};
-        new DualPivotQuicksort<String>().sort(input);
+        new InsertionSortMSD<>(InsertionSortMSD.getStringBinaryOperator(),0).sort(input);
         assertArrayEquals(expected, input);
     }
 
     @Test
     public void sortEnglish1(){
-        String[] input = "she sells seashells by the seashore the shells she sells are surely seashells".split(" ");
-        String[] expected = "are by seashells seashells seashore sells sells she she shells surely the the".split(" ");
-        new DualPivotQuicksort<String>().sort(input);
+        String[] input = "she sells seashells seashore shells she sells surely seashells".split(" ");
+        String[] expected = "seashells seashells seashore sells sells she she shells surely".split(" ");
+        new InsertionSortMSD<>(InsertionSortMSD.getStringBinaryOperator(),1).sort(input);
         assertArrayEquals(expected, input);
     }
 
     @Test
     public void sortEnglish2(){
-        String[] input = "she sells seashells seashore shells she sells surely seashells".split(" ");
-        String[] expected = "seashells seashells seashore sells sells she she shells surely".split(" ");
-        new DualPivotQuicksort<String>().sort(input);
+        String[] input = "sells seashells seashore sells seashells".split(" ");
+        String[] expected = "seashells seashells seashore sells sells".split(" ");
+        new InsertionSortMSD<>(InsertionSortMSD.getStringBinaryOperator(),2).sort(input);
         assertArrayEquals(expected, input);
     }
 
     @Test
-    public void sortChineese0() throws URISyntaxException, IOException {
+    public void sortChinese0() throws URISyntaxException, IOException {
         String[] k = FileReader.fileReader("shuffledChinese.txt");
         String[] a = FileReader.fileReader("sortedChinese.txt");
-        new DualPivotQuicksort<String>().withLocale(Locale.CHINA).sort(k);
+        new InsertionSortMSD<>(InsertionSortMSD.getPinYinStringBinaryOperator(),0).sort(k);
         assertArrayEquals(a, k);
     }
 
@@ -61,8 +62,17 @@ public class DualPivotQuickSortTest {
     public void sortTelugu0() {
         String[] k = new String[]{"శుభోదయం","నమస్కారం","రాత్రి","నమస్కారం","శుభోదయం"};
         String[] a = new String[]{"నమస్కారం","నమస్కారం","రాత్రి","శుభోదయం","శుభోదయం"};
-        new DualPivotQuicksort<String>().sort(k);
+        new InsertionSortMSD<>(InsertionSortMSD.getStringBinaryOperator(),0).sort(k);
         assertArrayEquals(a, k);
     }
+
+    @Test
+    public void sortTelugu1() {
+        String[] k = new String[]{"శుభోదయం","నమస్కారం","రాత్రి","నమస్కారం","శుభోదయం"};
+        String[] a = new String[]{"నమస్కారం","నమస్కారం","రాత్రి","శుభోదయం","శుభోదయం"};
+        new InsertionSortMSD<>(InsertionSortMSD.getStringBinaryOperator(),1).sort(k);
+        assertArrayEquals(a, k);
+    }
+
 
 }
