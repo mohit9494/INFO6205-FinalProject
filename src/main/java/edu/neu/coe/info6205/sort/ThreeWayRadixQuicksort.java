219aed54ef4698977f0610bbbb432edccdad5d86
saiakhil <saiakhil90@gmail.com>
2021-12-02 15:29:38 -0500

    Extend ThreeWayRadixQuicksort from SortAPI

--- src/main/java/edu/neu/coe/info6205/sort/ThreeWayRadixQuicksort.java
+++ src/main/java/edu/neu/coe/info6205/sort/ThreeWayRadixQuicksort.java
@@ -3,26 +3,35 @@ package edu.neu.coe.info6205.sort;
 import edu.neu.coe.info6205.util.SortUtils;
 import org.jetbrains.annotations.NotNull;
 
-public class ThreeWayRadixQuicksort extends SortAPI<String>{
+import java.util.function.BinaryOperator;
+
+public class ThreeWayRadixQuicksort<T extends Comparable<T>> extends SortAPI<T>{
+
+    public ThreeWayRadixQuicksort(){
+        this((item,d)->d);
+    }
+
+    public ThreeWayRadixQuicksort(BinaryOperator<T> binaryOperator){
+        this.withBinaryOperator(binaryOperator);
+    }
 
     @Override
-    public void sort(@NotNull String[] arr, int from, int to) {
+    public void sort(@NotNull T[] arr, int from, int to) {
         this.sort(arr, from, to,0);
     }
 
     @Override
-    protected void sort(@NotNull String[] arr, int from, int to, int d) {
+    protected void sort(@NotNull T[] arr, int from, int to, int d) {
         if (to <= from) return;
 
         int lt = from;
 
         int gt = to;
         int i = from;
-        int v = charAt(arr[from], d);
+        int v = Integer.parseInt(this.getBinaryOperator().apply(arr[from],(T)String.valueOf(d)).toString());
 
         while (i <= gt) {
-            int t = charAt(arr[i], d);
-
+            int t = Integer.parseInt(this.getBinaryOperator().apply(arr[i],(T)String.valueOf(d)).toString());
             if (t == v) i++;
             else if (t < v) this.swap(arr, i++, lt++);
             else this.swap(arr, i, gt--);
@@ -33,44 +42,23 @@ public class ThreeWayRadixQuicksort extends SortAPI<String>{
         this.sort(arr, gt + 1, to, d);
     }
 
-    private static int charAt(String s, int d) {
-        String pinyin = SortUtils.getPinYinString(s);
-        if (d < pinyin.length()) return pinyin.charAt(d);
-        else return -1;
-    /*    if (d < s.length()) {
-            System.out.println("Char ====> " + s.charAt(d) + " Ascii ===> " + (int) s.charAt(d));
-            return s.charAt(d);
-        } else return -1;*/
+    public static BinaryOperator<String> getPinyinCharOperator(){
+        return (item,d)->{
+            int tempD = Integer.parseInt(d);
+            String pinyin = SortUtils.getPinYinString(item);
+            if (tempD < pinyin.length()) return String.valueOf((int)pinyin.charAt(tempD));
+            return "-1";
+        };
     }
 
-//    private static void swap(String[] a, int i, int j) {
-//        String temp = a[i];
-//        a[i] = a[j];
-//        a[j] = temp;
-//    }
+    public static BinaryOperator<String> getStringCharOperator(){
+        return (item,d)->{
+            int tempD = Integer.parseInt(d);
+            if (tempD < item.length()) return String.valueOf((int)item.charAt(tempD));
+            return "-1";
+        };
+    }
 
-//    private static void sort1(String[] arr, int from, int to, int d) {
-//
-//        // 3 way partitioning (using dth character)
-//        if (to <= from) return;
-//        int lt = from;
-//        int gt = to;
-//        int i = from;
-//        int v = charAt(arr[from], d);
-//
-//        while (i <= gt) {
-//            int t = charAt(arr[i], d);
-//
-//            if (t == v) i++;
-//            else if (t < v) swap(arr, i++, lt++);
-//            else if (t > v) swap(arr, i, gt--);
-//        }
-//
-//        sort(arr, from, lt - 1, d);
-//        if (v >= 0) sort(arr, lt, gt, d + 1); // look here how u skipped dth character wow!!!!
-//        sort(arr, gt + 1, to, d);
-//
-//    }
 
 
 }
