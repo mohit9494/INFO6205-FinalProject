d19af899388bc03b3985096b342a961ba31af71c
saiakhil <saiakhil90@gmail.com>
2021-12-03 10:15:57 -0500

    Extend LSDStringSort from SortAPI

--- src/main/java/edu/neu/coe/info6205/sort/LSDStringSort.java
+++ src/main/java/edu/neu/coe/info6205/sort/LSDStringSort.java
@@ -1,95 +1,52 @@
 package edu.neu.coe.info6205.sort;
 
 import edu.neu.coe.info6205.util.SortUtils;
+import org.jetbrains.annotations.NotNull;
 
-public class LSDStringSort {
 
-    private final int ASCII_RANGE = 65536;
+public class LSDStringSort extends SortAPI<String> {
 
-    /**
-     * findMaxLength method returns maximum length of all available strings in an array
-     *
-     * @param strArr It contains an array of String from which maximum length needs to be found
-     * @return int Returns maximum length value
-     */
-    private int findMaxLength(String[] strArr) {
-        int maxLength = strArr[0].length();
-        for (String str : strArr)
-            maxLength = Math.max(maxLength, str.length());
-        return maxLength;
+    private static final int ASCII_RANGE = 65536;
+
+    private int getCharAtPosition(String str, int charPosition) {
+        String currentStr = this.getStringBasedOnLocale(str);
+        return (charPosition >= currentStr.length()) ? 0 : currentStr.charAt(charPosition);
     }
 
-    /**
-     * charAsciiVal method returns ASCII value of particular character in a String.
-     *
-     * @param str          String input for which ASCII Value need to be found
-     * @param charPosition Character position of which ASCII value needs to be found. If character
-     *                     doesn't exist then ASCII value of null i.e. 0 is returned
-     * @return int Returns ASCII value
-     */
-    private int charAsciiVal(String str, int charPosition) {
-
-        String pinyin = SortUtils.getPinYinString(str);
-
-        if (charPosition >= pinyin.length()) {
-            return 0;
-        }
-        return pinyin.charAt(charPosition);
-        // return str.charAt(charPosition);
-    }
-
-    /**
-     * charSort method is implementation of LSD sort algorithm at particular character.
-     *
-     * @param strArr       It contains an array of String on which LSD char sort needs to be performed
-     * @param charPosition This is the character position on which sort would be performed
-     * @param from         This is the starting index from which sorting operation will begin
-     * @param to           This is the ending index up until which sorting operation will be continued
-     */
     private void charSort(String[] strArr, int charPosition, int from, int to) {
         int[] count = new int[ASCII_RANGE + 2];
         String[] result = new String[strArr.length];
 
         for (int i = from; i <= to; i++) {
-            int c = charAsciiVal(strArr[i], charPosition);
+            int c = this.getCharAtPosition(strArr[i], charPosition);
             count[c + 2]++;
         }
 
-        // transform counts to indices
-        for (int r = 1; r < ASCII_RANGE + 2; r++)
-            count[r] += count[r - 1];
+        for (int r = 1; r < ASCII_RANGE + 2; r++) count[r] += count[r - 1];
 
-        // distribute
         for (int i = from; i <= to; i++) {
-            int c = charAsciiVal(strArr[i], charPosition);
+            int c = this.getCharAtPosition(strArr[i], charPosition);
             result[count[c + 1]++] = strArr[i];
         }
 
-        // copy back
         if (to + 1 - from >= 0) System.arraycopy(result, 0, strArr, from, to + 1 - from);
     }
 
-    /**
-     * sort method is implementation of LSD String sort algorithm.
-     *
-     * @param strArr It contains an array of String on which LSD sort needs to be performed
-     * @param from   This is the starting index from which sorting operation will begin
-     * @param to     This is the ending index up until which sorting operation will be continued
-     */
     public void sort(String[] strArr, int from, int to) {
-        // int maxLength = findMaxLength(strArr);
-        // finding max length of chinese pinyin character
-        int maxLength = SortUtils.findMaxLengthPinyin(strArr);
-        for (int i = maxLength - 1; i >= 0; i--)
-            charSort(strArr, i, from, to);
+        int maxLength = this.getMaxStringLength(strArr);
+        for (int i = maxLength - 1; i >= 0; i--) charSort(strArr, i, from, to);
     }
 
-    /**
-     * sort method is implementation of LSD String sort algorithm.
-     *
-     * @param strArr It contains an array of String on which LSD sort needs to be performed
-     */
-    public void sort(String[] strArr) {
-        sort(strArr, 0, strArr.length - 1);
+    private int getMaxStringLength(@NotNull String[] strArray) {
+        int maxLength = 0;
+        for (String s : strArray) {
+            maxLength = Math.max(maxLength, this.getStringBasedOnLocale(s).length());
+        }
+        return maxLength;
     }
+
+    private String getStringBasedOnLocale(@NotNull String str) {
+        return (this.getLocale() == null) ? str : SortUtils.getPinYinString(str);
+    }
+
 }
