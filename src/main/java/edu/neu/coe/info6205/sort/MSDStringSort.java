93c749fc851932b83a6d07ff21c83c3992bda05d
saiakhil <saiakhil90@gmail.com>
2021-12-03 15:25:56 -0500

    Inherit MSDStringSort from StringSortAPI

--- src/main/java/edu/neu/coe/info6205/sort/MSDStringSort.java
+++ src/main/java/edu/neu/coe/info6205/sort/MSDStringSort.java
@@ -1,6 +1,6 @@
 package edu.neu.coe.info6205.sort;
 
-import edu.neu.coe.info6205.sort.api.SortAPI;
+import edu.neu.coe.info6205.sort.api.StringSortAPI;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.BinaryOperator;
@@ -8,22 +8,18 @@ import java.util.function.BinaryOperator;
 /**
  * Class to implement Most significant digit string sort (a radix sort).
  */
-public class MSDStringSort extends SortAPI<String> {
+public class MSDStringSort extends StringSortAPI {
 
     private final int radix = 65536;
-    private  int cutoff = 15;
+    private int cutoff = 15;
     private final String[] aux;       // auxiliary array for distribution
 
     private BinaryOperator<String> insertionSortMSDOperator = null;
 
     public MSDStringSort(int n) {
-        this(n,(item,d)->item);
-    }
-
-    public MSDStringSort(int n, BinaryOperator<String> binaryOperator){
-        this.withBinaryOperator(binaryOperator);
         this.aux = new String[n];
     }
+
 
     public MSDStringSort withInsertionSortMSDOperator(BinaryOperator<String> insertionSortMSDOperator){
         this.insertionSortMSDOperator = insertionSortMSDOperator;
@@ -34,47 +30,6 @@ public class MSDStringSort extends SortAPI<String> {
         this.cutoff = cutoff;
         return this;
     }
-
-    /**
-     * Sort an array of Strings using MSDStringSort.
-     *
-     * @param a the array to be sorted.
-     */
-//    public static void sort(String[] a) {
-//        int n = a.length;
-//        aux = new String[n];
-//        sort(a, 0, n, 0);
-//    }
-
-    /**
-     * Sort from a[lo] to a[hi] (exclusive), ignoring the first d characters of each String.
-     * This method is recursive.
-     *
-     * @param a  the array to be sorted.
-     * @param lo the low index.
-     * @param hi the high index (one above the highest actually processed).
-     * @param d  the number of characters in each String to be skipped.
-     */
-    //private static void sort(String[] a, int lo, int hi, int d) {
-    // if (hi < lo + cutoff) InsertionSortMSD.sort(a, lo, hi, d);
-//        else {
-//            int[] count = new int[radix + 2];        // Compute frequency counts.
-//            for (int i = lo; i < hi; i++)
-//                count[charAt(a[i], d) + 2]++;
-//            for (int r = 0; r < radix + 1; r++)      // Transform counts to indices.
-//                count[r + 1] += count[r];
-//            for (int i = lo; i < hi; i++)     // Distribute.
-//                aux[count[charAt(a[i], d) + 1]++] = a[i];
-//            // Copy back.
-//            if (hi - lo >= 0) System.arraycopy(aux, 0, a, lo, hi - lo);
-//
-//            // Recursively sort for each character value.
-//            for (int r = 0; r < radix; r++) {
-//                sort(a, lo + count[r], lo + count[r + 1] - 1, d + 1);
-//            }
-//        }
-    // }
-
 
     @Override
     protected void sort(@NotNull String[] t, int from, int to) {
@@ -88,15 +43,13 @@ public class MSDStringSort extends SortAPI<String> {
         else {
             int[] count = new int[radix + 2];        // Compute frequency counts.
             for (int i = from; i <= to; i++)
-                count[Integer.parseInt(this.getBinaryOperator().apply(arr[i],String.valueOf(d))) + 2]++;
+                count[this.getCharAtPosition(arr[i], d, this.getUnaryOperator()) + 2]++;
             for (int r = 0; r < radix + 1; r++)      // Transform counts to indices.
                 count[r + 1] += count[r];
             for (int i = from; i <= to; i++)     // Distribute.
-                aux[count[Integer.parseInt(this.getBinaryOperator().apply(arr[i],String.valueOf(d))) + 1]++] = arr[i];
+                aux[count[this.getCharAtPosition(arr[i], d, this.getUnaryOperator()) + 1]++] = arr[i];
             // Copy back.
-            for (int i = from; i <= to; i++) {
-                arr[i] =  aux[i - from];
-            }
+            if (to + 1 - from >= 0) System.arraycopy(aux, 0, arr, from, to + 1 - from);
 
             // Recursively sort for each character value.
             for (int r = 0; r < radix; r++) {
--- src/test/java/edu/neu/coe/info6205/sort/MSDStringSortTest.java
+++ src/test/java/edu/neu/coe/info6205/sort/MSDStringSortTest.java
@@ -1,5 +1,6 @@
 package edu.neu.coe.info6205.sort;
 
+import edu.neu.coe.info6205.sort.api.StringSortAPI;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -12,31 +13,31 @@ import static org.junit.Assert.assertArrayEquals;
  */
 public class MSDStringSortTest {
     @Test
-    public void sortEnglish0(){
-        String[] input = new String[]{"c","b","a","f","d","e"};
-        String[] expected = new String[]{"a","b","c","d","e","f"};
-        new MSDStringSort(input.length,ThreeWayRadixQuicksort.getStringCharOperator())
-                .withInsertionSortMSDOperator(InsertionSortMSD.getStringBinaryOperator())
+    public void sortEnglish0() {
+        String[] input = new String[]{"c", "b", "a", "f", "d", "e"};
+        String[] expected = new String[]{"a", "b", "c", "d", "e", "f"};
+        new MSDStringSort(input.length)
+                .withInsertionSortMSDOperator(InsertionSortMSD.SUBSTRING_BINARY_OPERATOR)
                 .sort(input);
         assertArrayEquals(expected, input);
     }
 
     @Test
-    public void sortEnglish1(){
+    public void sortEnglish1() {
         String[] input = "she sells seashells by the seashore the shells she sells are surely seashells".split(" ");
         String[] expected = "are by seashells seashells seashore sells sells she she shells surely the the".split(" ");
-        new MSDStringSort(input.length,ThreeWayRadixQuicksort.getStringCharOperator())
-                .withInsertionSortMSDOperator(InsertionSortMSD.getStringBinaryOperator())
+        new MSDStringSort(input.length)
+                .withInsertionSortMSDOperator(InsertionSortMSD.SUBSTRING_BINARY_OPERATOR)
                 .sort(input);
         assertArrayEquals(expected, input);
     }
 
     @Test
-    public void sortEnglish2(){
+    public void sortEnglish2() {
         String[] input = "she sells seashells seashore shells she sells surely seashells".split(" ");
         String[] expected = "seashells seashells seashore sells sells she she shells surely".split(" ");
-        new MSDStringSort(input.length,ThreeWayRadixQuicksort.getStringCharOperator())
-                .withInsertionSortMSDOperator(InsertionSortMSD.getStringBinaryOperator())
+        new MSDStringSort(input.length)
+                .withInsertionSortMSDOperator(InsertionSortMSD.SUBSTRING_BINARY_OPERATOR)
                 .sort(input);
         assertArrayEquals(expected, input);
     }
@@ -46,8 +47,9 @@ public class MSDStringSortTest {
         String[] expected = new String[]{"阿安","阿彬","阿兵","阿冰冰","阿晨","阿称","阿丛","阿丹丹","阿德力","阿迪迪","阿迪江","阿迪雅","阿栋","阿繁","阿方","阿菲","阿飞儿","阿飞飞","阿芬","阿丰","阿夏","阿鲜","阿湘","阿晓","阿新","阿行","阿旭","阿轩","乔建雄","乔建业","乔建永","乔建云","乔建珍","乔建忠","乔江","乔江涛","乔娇","乔娇娇"};
         String[] input = Arrays.copyOf(expected,expected.length);
         Collections.shuffle(Arrays.asList(input));
-        new MSDStringSort(expected.length, ThreeWayRadixQuicksort.getPinyinCharOperator())
+        new MSDStringSort(expected.length)
                 .withInsertionSortMSDOperator(InsertionSortMSD.PINYIN_CHAR_SUBSTRING_BINARY_OPERATOR)
+                .withUnaryOperator(StringSortAPI.PINYIN_STRING_SUPPLIER)
                 .sort(input);
         assertArrayEquals(expected, input);
     }
@@ -57,8 +59,9 @@ public class MSDStringSortTest {
         String[] expected = new String[]{"阿彬","阿兵","阿称","阿栋","阿菲","阿芬","阿丰","阿晓","阿新","乔江","乔江涛","乔娇","乔娇娇"};
         String[] input = Arrays.copyOf(expected,expected.length);
         Collections.shuffle(Arrays.asList(input));
-        new MSDStringSort(expected.length, ThreeWayRadixQuicksort.getPinyinCharOperator())
+        new MSDStringSort(expected.length)
                 .withInsertionSortMSDOperator(InsertionSortMSD.PINYIN_CHAR_SUBSTRING_BINARY_OPERATOR)
+                .withUnaryOperator(StringSortAPI.PINYIN_STRING_SUPPLIER)
                 .sort(input);
         assertArrayEquals(expected, input);
     }
@@ -68,8 +71,9 @@ public class MSDStringSortTest {
         String[] expected = new String[]{"阿安","阿彬","阿兵","阿冰冰","阿晨","阿称","阿丛","阿栋","阿繁","阿方","阿菲","阿丰","阿夏","阿鲜","阿行","阿旭","阿轩","乔建雄","乔建珍","乔建忠","乔江","乔江涛","乔娇","乔娇娇"};
         String[] input = Arrays.copyOf(expected,expected.length);
         Collections.shuffle(Arrays.asList(input));
-        new MSDStringSort(expected.length, ThreeWayRadixQuicksort.getPinyinCharOperator())
+        new MSDStringSort(expected.length)
                 .withInsertionSortMSDOperator(InsertionSortMSD.PINYIN_CHAR_SUBSTRING_BINARY_OPERATOR)
+                .withUnaryOperator(StringSortAPI.PINYIN_STRING_SUPPLIER)
                 .sort(input);
         assertArrayEquals(expected, input);
     }
@@ -77,33 +81,33 @@ public class MSDStringSortTest {
 
     @Test
     public void sortTelugu0() {
-        String[] expected = new String[]{"అంటూ","అడగ్గా","అతిథిగా","అది","అనిల్","అనిల్","అన్నారు","అన్‌స్టాపబుల్‌","అలరించార","ఆట","ఆడుకున్నారు","ఇద్దరితో","ఈ","ఈ","ఈ","ఎందుకో","ఏది","ఓ","ఓ","ఓ","కార్యక్రమ","కార్యక్రమానికి","కోప్పడ్డారు","చూపించి","చేశారు","తనకెంత","తీసుకొచ్చిన","దర్శకుడు","దీన్ని","దేనికి","నటుడు","నవ్వులు","పంచారు","పడితే","పేరు","ఫొటోను","బాలకృష్ణ","బాలకృష్ణ","బాలకృష్ణ","బాలకృష్ణ","బ్రహ్మానందం","బ్రహ్మానందం","బ్రహ్మానందంపై","మరో","మాంచి","రావిపూడి","రావిపూడి","విచ్చేసి","వినోదం","సంభాషించారు","సమాధానం"};
-        String[] input = Arrays.copyOf(expected,expected.length);
+        String[] expected = new String[]{"అంటూ", "అడగ్గా", "అతిథిగా", "అది", "అనిల్", "అనిల్", "అన్నారు", "అన్‌స్టాపబుల్‌", "అలరించార", "ఆట", "ఆడుకున్నారు", "ఇద్దరితో", "ఈ", "ఈ", "ఈ", "ఎందుకో", "ఏది", "ఓ", "ఓ", "ఓ", "కార్యక్రమ", "కార్యక్రమానికి", "కోప్పడ్డారు", "చూపించి", "చేశారు", "తనకెంత", "తీసుకొచ్చిన", "దర్శకుడు", "దీన్ని", "దేనికి", "నటుడు", "నవ్వులు", "పంచారు", "పడితే", "పేరు", "ఫొటోను", "బాలకృష్ణ", "బాలకృష్ణ", "బాలకృష్ణ", "బాలకృష్ణ", "బ్రహ్మానందం", "బ్రహ్మానందం", "బ్రహ్మానందంపై", "మరో", "మాంచి", "రావిపూడి", "రావిపూడి", "విచ్చేసి", "వినోదం", "సంభాషించారు", "సమాధానం"};
+        String[] input = Arrays.copyOf(expected, expected.length);
         Collections.shuffle(Arrays.asList(input));
-        new MSDStringSort(expected.length,ThreeWayRadixQuicksort.getStringCharOperator())
-                .withInsertionSortMSDOperator(InsertionSortMSD.getStringBinaryOperator())
+        new MSDStringSort(expected.length)
+                .withInsertionSortMSDOperator(InsertionSortMSD.SUBSTRING_BINARY_OPERATOR)
                 .sort(input);
         assertArrayEquals(expected, input);
     }
 
     @Test
     public void sortTeluguInsertionCut1() {
-        String[] expected = new String[]{"అన్‌స్టాపబుల్‌","అలరించార","ఆట","ఆడుకున్నారు","ఇద్దరితో","ఈ","చూపించి","దేనికి","నటుడు","నవ్వులు","మరో","మాంచి","విచ్చేసి","సమాధానం"};
-        String[] input = Arrays.copyOf(expected,expected.length);
+        String[] expected = new String[]{"అన్‌స్టాపబుల్‌", "అలరించార", "ఆట", "ఆడుకున్నారు", "ఇద్దరితో", "ఈ", "చూపించి", "దేనికి", "నటుడు", "నవ్వులు", "మరో", "మాంచి", "విచ్చేసి", "సమాధానం"};
+        String[] input = Arrays.copyOf(expected, expected.length);
         Collections.shuffle(Arrays.asList(input));
-        new MSDStringSort(expected.length,ThreeWayRadixQuicksort.getStringCharOperator())
-                .withInsertionSortMSDOperator(InsertionSortMSD.getStringBinaryOperator())
+        new MSDStringSort(expected.length)
+                .withInsertionSortMSDOperator(InsertionSortMSD.SUBSTRING_BINARY_OPERATOR)
                 .sort(input);
         assertArrayEquals(expected, input);
     }
 
     @Test
     public void sortTelugu2() {
-        String[] expected = new String[]{"అనిల్","అన్నారు","అన్‌స్టాపబుల్‌","అలరించార","ఆట","ఆడుకున్నారు","ఇద్దరితో","ఈ","ఈ","ఈ","ఎందుకో","ఏది","ఓ","ఓ","ఓ","చూపించి","చేశారు","తనకెంత","తీసుకొచ్చిన","దర్శకుడు","దీన్ని","దేనికి","నటుడు","నవ్వులు","పంచారు","పడితే","బాలకృష్ణ","బాలకృష్ణ","బ్రహ్మానందం","బ్రహ్మానందం","బ్రహ్మానందంపై","మరో","మాంచి","రావిపూడి","రావిపూడి","విచ్చేసి","వినోదం","సంభాషించారు","సమాధానం"};
-        String[] input = Arrays.copyOf(expected,expected.length);
+        String[] expected = new String[]{"అనిల్", "అన్నారు", "అన్‌స్టాపబుల్‌", "అలరించార", "ఆట", "ఆడుకున్నారు", "ఇద్దరితో", "ఈ", "ఈ", "ఈ", "ఎందుకో", "ఏది", "ఓ", "ఓ", "ఓ", "చూపించి", "చేశారు", "తనకెంత", "తీసుకొచ్చిన", "దర్శకుడు", "దీన్ని", "దేనికి", "నటుడు", "నవ్వులు", "పంచారు", "పడితే", "బాలకృష్ణ", "బాలకృష్ణ", "బ్రహ్మానందం", "బ్రహ్మానందం", "బ్రహ్మానందంపై", "మరో", "మాంచి", "రావిపూడి", "రావిపూడి", "విచ్చేసి", "వినోదం", "సంభాషించారు", "సమాధానం"};
+        String[] input = Arrays.copyOf(expected, expected.length);
         Collections.shuffle(Arrays.asList(input));
-        new MSDStringSort(expected.length,ThreeWayRadixQuicksort.getStringCharOperator())
-                .withInsertionSortMSDOperator(InsertionSortMSD.getStringBinaryOperator())
+        new MSDStringSort(expected.length)
+                .withInsertionSortMSDOperator(InsertionSortMSD.SUBSTRING_BINARY_OPERATOR)
                 .sort(input);
         assertArrayEquals(expected, input);
     }
