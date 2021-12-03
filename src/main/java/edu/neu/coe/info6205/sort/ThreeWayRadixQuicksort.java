e593bddf16bbf6c74fe59f455e10dae6e1a09948
saiakhil <saiakhil90@gmail.com>
2021-12-03 16:06:00 -0500

    Code CleanUp - Optimize code

--- src/main/java/edu/neu/coe/info6205/sort/ThreeWayRadixQuicksort.java
+++ src/main/java/edu/neu/coe/info6205/sort/ThreeWayRadixQuicksort.java
@@ -1,20 +1,10 @@
 package edu.neu.coe.info6205.sort;
 
 import edu.neu.coe.info6205.sort.api.SortAPI;
-import edu.neu.coe.info6205.util.SortUtils;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.function.BinaryOperator;
 
 public class ThreeWayRadixQuicksort<T extends Comparable<T>> extends SortAPI<T> {
-
-    public ThreeWayRadixQuicksort() {
-        this((item, d) -> d);
-    }
-
-    public ThreeWayRadixQuicksort(BinaryOperator<T> binaryOperator) {
-        this.withBinaryOperator(binaryOperator);
-    }
 
     @Override
     public void sort(@NotNull T[] arr, int from, int to) {
@@ -29,10 +19,10 @@ public class ThreeWayRadixQuicksort<T extends Comparable<T>> extends SortAPI<T> 
 
         int gt = to;
         int i = from;
-        int v = Integer.parseInt(this.getBinaryOperator().apply(arr[from],(T)String.valueOf(d)).toString());
+        int v = this.getCharAtPosition(arr[from], d, this.getUnaryOperator());
 
         while (i <= gt) {
-            int t = Integer.parseInt(this.getBinaryOperator().apply(arr[i],(T)String.valueOf(d)).toString());
+            int t = this.getCharAtPosition(arr[i], d, this.getUnaryOperator());
             if (t == v) i++;
             else if (t < v) this.swap(arr, i++, lt++);
             else this.swap(arr, i, gt--);
@@ -42,24 +32,5 @@ public class ThreeWayRadixQuicksort<T extends Comparable<T>> extends SortAPI<T> 
         if (v >= 0) this.sort(arr, lt, gt, d + 1); // look here how u skipped dth character wow!!!!
         this.sort(arr, gt + 1, to, d);
     }
-
-    public static BinaryOperator<String> getPinyinCharOperator(){
-        return (item,d)->{
-            int tempD = Integer.parseInt(d);
-            String pinyin = SortUtils.getPinYinString(item);
-            if (tempD < pinyin.length()) return String.valueOf((int)pinyin.charAt(tempD));
-            return "-1";
-        };
-    }
-
-    public static BinaryOperator<String> getStringCharOperator(){
-        return (item,d)->{
-            int tempD = Integer.parseInt(d);
-            if (tempD < item.length()) return String.valueOf((int)item.charAt(tempD));
-            return "-1";
-        };
-    }
-
-
 
 }
--- src/test/java/edu/neu/coe/info6205/sort/ThreeWayRadixQuickSortTest.java
+++ src/test/java/edu/neu/coe/info6205/sort/ThreeWayRadixQuickSortTest.java
@@ -1,5 +1,6 @@
 package edu.neu.coe.info6205.sort;
 
+import edu.neu.coe.info6205.sort.api.StringSortAPI;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -16,7 +17,7 @@ public class ThreeWayRadixQuickSortTest {
     public void sortEnglish0(){
         String[] input = new String[]{"c","b","a","f","d","e"};
         String[] expected = new String[]{"a","b","c","d","e","f"};
-        new ThreeWayRadixQuicksort<>(ThreeWayRadixQuicksort.getStringCharOperator()).sort(input);
+        new ThreeWayRadixQuicksort<String>().sort(input);
         assertArrayEquals(expected, input);
     }
 
@@ -24,7 +25,7 @@ public class ThreeWayRadixQuickSortTest {
     public void sortEnglish1(){
         String[] input = "she sells seashells by the seashore the shells she sells are surely seashells".split(" ");
         String[] expected = "are by seashells seashells seashore sells sells she she shells surely the the".split(" ");
-        new ThreeWayRadixQuicksort<>(ThreeWayRadixQuicksort.getStringCharOperator()).sort(input);
+        new ThreeWayRadixQuicksort<String>().sort(input);
         assertArrayEquals(expected, input);
     }
 
@@ -32,35 +33,41 @@ public class ThreeWayRadixQuickSortTest {
     public void sortEnglish2(){
         String[] input = "she sells seashells seashore shells she sells surely seashells".split(" ");
         String[] expected = "seashells seashells seashore sells sells she she shells surely".split(" ");
-        new ThreeWayRadixQuicksort<>(ThreeWayRadixQuicksort.getStringCharOperator()).sort(input);
+        new ThreeWayRadixQuicksort<String>().sort(input);
         assertArrayEquals(expected, input);
     }
 
 
     @Test
-    public void sortChinese0(){
-        String[] expected = new String[]{"阿安","阿彬","阿兵","阿冰冰","阿晨","阿称","阿丛","阿丹丹","阿德力","阿迪迪","阿迪江","阿迪雅","阿栋","阿繁","阿方","阿菲","阿飞儿","阿飞飞","阿芬","阿丰","阿夏","阿鲜","阿湘","阿晓","阿新","阿行","阿旭","阿轩","乔建雄","乔建业","乔建永","乔建云","乔建珍","乔建忠","乔江","乔江涛","乔娇","乔娇娇"};
-        String[] input = Arrays.copyOf(expected,expected.length);
+    public void sortChinese0() {
+        String[] expected = new String[]{"阿安", "阿彬", "阿兵", "阿冰冰", "阿晨", "阿称", "阿丛", "阿丹丹", "阿德力", "阿迪迪", "阿迪江", "阿迪雅", "阿栋", "阿繁", "阿方", "阿菲", "阿飞儿", "阿飞飞", "阿芬", "阿丰", "阿夏", "阿鲜", "阿湘", "阿晓", "阿新", "阿行", "阿旭", "阿轩", "乔建雄", "乔建业", "乔建永", "乔建云", "乔建珍", "乔建忠", "乔江", "乔江涛", "乔娇", "乔娇娇"};
+        String[] input = Arrays.copyOf(expected, expected.length);
         Collections.shuffle(Arrays.asList(input));
-        new ThreeWayRadixQuicksort<>(ThreeWayRadixQuicksort.getPinyinCharOperator()).sort(input);
+        new ThreeWayRadixQuicksort<String>()
+                .withUnaryOperator(StringSortAPI.PINYIN_STRING_SUPPLIER)
+                .sort(input);
         assertArrayEquals(expected, input);
     }
 
     @Test
-    public void sortChineseInsertion1(){
-        String[] expected = new String[]{"阿彬","阿兵","阿称","阿栋","阿菲","阿芬","阿丰","阿晓","阿新","乔江","乔江涛","乔娇","乔娇娇"};
-        String[] input = Arrays.copyOf(expected,expected.length);
+    public void sortChineseInsertion1() {
+        String[] expected = new String[]{"阿彬", "阿兵", "阿称", "阿栋", "阿菲", "阿芬", "阿丰", "阿晓", "阿新", "乔江", "乔江涛", "乔娇", "乔娇娇"};
+        String[] input = Arrays.copyOf(expected, expected.length);
         Collections.shuffle(Arrays.asList(input));
-        new ThreeWayRadixQuicksort<>(ThreeWayRadixQuicksort.getPinyinCharOperator()).sort(input);
+        new ThreeWayRadixQuicksort<String>()
+                .withUnaryOperator(StringSortAPI.PINYIN_STRING_SUPPLIER)
+                .sort(input);
         assertArrayEquals(expected, input);
     }
 
     @Test
-    public void sortChinese2(){
-        String[] expected = new String[]{"阿安","阿彬","阿兵","阿冰冰","阿晨","阿称","阿丛","阿栋","阿繁","阿方","阿菲","阿丰","阿夏","阿鲜","阿行","阿旭","阿轩","乔建雄","乔建珍","乔建忠","乔江","乔江涛","乔娇","乔娇娇"};
-        String[] input = Arrays.copyOf(expected,expected.length);
+    public void sortChinese2() {
+        String[] expected = new String[]{"阿安", "阿彬", "阿兵", "阿冰冰", "阿晨", "阿称", "阿丛", "阿栋", "阿繁", "阿方", "阿菲", "阿丰", "阿夏", "阿鲜", "阿行", "阿旭", "阿轩", "乔建雄", "乔建珍", "乔建忠", "乔江", "乔江涛", "乔娇", "乔娇娇"};
+        String[] input = Arrays.copyOf(expected, expected.length);
         Collections.shuffle(Arrays.asList(input));
-        new ThreeWayRadixQuicksort<>(ThreeWayRadixQuicksort.getPinyinCharOperator()).sort(input);
+        new ThreeWayRadixQuicksort<String>()
+                .withUnaryOperator(StringSortAPI.PINYIN_STRING_SUPPLIER)
+                .sort(input);
         assertArrayEquals(expected, input);
     }
 
@@ -69,7 +76,7 @@ public class ThreeWayRadixQuickSortTest {
         String[] expected = new String[]{"అంటూ","అడగ్గా","అతిథిగా","అది","అనిల్","అనిల్","అన్నారు","అన్‌స్టాపబుల్‌","అలరించార","ఆట","ఆడుకున్నారు","ఇద్దరితో","ఈ","ఈ","ఈ","ఎందుకో","ఏది","ఓ","ఓ","ఓ","కార్యక్రమ","కార్యక్రమానికి","కోప్పడ్డారు","చూపించి","చేశారు","తనకెంత","తీసుకొచ్చిన","దర్శకుడు","దీన్ని","దేనికి","నటుడు","నవ్వులు","పంచారు","పడితే","పేరు","ఫొటోను","బాలకృష్ణ","బాలకృష్ణ","బాలకృష్ణ","బాలకృష్ణ","బ్రహ్మానందం","బ్రహ్మానందం","బ్రహ్మానందంపై","మరో","మాంచి","రావిపూడి","రావిపూడి","విచ్చేసి","వినోదం","సంభాషించారు","సమాధానం"};
         String[] input = Arrays.copyOf(expected,expected.length);
         Collections.shuffle(Arrays.asList(input));
-        new ThreeWayRadixQuicksort<>(ThreeWayRadixQuicksort.getStringCharOperator()).sort(input);
+        new ThreeWayRadixQuicksort<String>().sort(input);
         assertArrayEquals(expected, input);
     }
 
@@ -78,7 +85,7 @@ public class ThreeWayRadixQuickSortTest {
         String[] expected = new String[]{"అన్‌స్టాపబుల్‌","అలరించార","ఆట","ఆడుకున్నారు","ఇద్దరితో","ఈ","చూపించి","దేనికి","నటుడు","నవ్వులు","మరో","మాంచి","విచ్చేసి","సమాధానం"};
         String[] input = Arrays.copyOf(expected,expected.length);
         Collections.shuffle(Arrays.asList(input));
-        new ThreeWayRadixQuicksort<>(ThreeWayRadixQuicksort.getStringCharOperator()).sort(input);
+        new ThreeWayRadixQuicksort<String>().sort(input);
         assertArrayEquals(expected, input);
     }
 
@@ -87,7 +94,7 @@ public class ThreeWayRadixQuickSortTest {
         String[] expected = new String[]{"అనిల్","అన్నారు","అన్‌స్టాపబుల్‌","అలరించార","ఆట","ఆడుకున్నారు","ఇద్దరితో","ఈ","ఈ","ఈ","ఎందుకో","ఏది","ఓ","ఓ","ఓ","చూపించి","చేశారు","తనకెంత","తీసుకొచ్చిన","దర్శకుడు","దీన్ని","దేనికి","నటుడు","నవ్వులు","పంచారు","పడితే","బాలకృష్ణ","బాలకృష్ణ","బ్రహ్మానందం","బ్రహ్మానందం","బ్రహ్మానందంపై","మరో","మాంచి","రావిపూడి","రావిపూడి","విచ్చేసి","వినోదం","సంభాషించారు","సమాధానం"};
         String[] input = Arrays.copyOf(expected,expected.length);
         Collections.shuffle(Arrays.asList(input));
-        new ThreeWayRadixQuicksort<>(ThreeWayRadixQuicksort.getStringCharOperator()).sort(input);
+        new ThreeWayRadixQuicksort<String>().sort(input);
         assertArrayEquals(expected, input);
     }
 
